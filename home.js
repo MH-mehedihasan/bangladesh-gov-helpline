52cf1e5385abecc5901d37c300b01771a2fef2bc
MH-mehedihasan <mehedihasan52299@gmail.com>
2025-10-08 22:14:25 +0600

    git push

--- home.js
+++ home.js
@@ -1,1 +1,26 @@
 
+
+document.getElementById('national-emergency-btn').addEventListener('click',function(){
+    alert('Calling National Emergency Number 999')
+})
+document.getElementById('police-call-btn').addEventListener('click',function(){
+    alert('Police Helpline Helpline Number 999')
+})
+document.getElementById('Fire-service-btn').addEventListener('click',function(){
+    alert('Calling National Emergency Number 999')
+})
+document.getElementById('ambulance-service-btn').addEventListener('click',function(){
+    alert('Ambulance Service Number 1994-999999')
+})
+document.getElementById('electriciy-help-btn').addEventListener('click',function(){
+    alert('Electricity Helpline 16216')
+})
+document.getElementById('anticoruption-btn').addEventListener('click',function(){
+    alert('anticoruption-btn Number 106')
+})
+document.getElementById('brac-helpline-btn').addEventListener('click',function(){
+    alert('Brac Helpliney Number 16445')
+})
+document.getElementById('railway-helpline-btn').addEventListener('click',function(){
+    alert('Bangladesh Railway Helpline 163')
+})
